d30016c40932cbd59575d0e030081fbcc094f5d7
joel <alcantara0729@gmail.com>
2018-06-27 08:21:32 -0400

    Add project files.

---
+++ SaludPublica/Models/Enfermo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaludPublica.Models
+{
+    public class Enfermo
+    {
+        public int EnfermoID { get; set; }
+        public ICollection<Enfermedad> Enfermedades { get; set; }
+        public virtual ICollection<Paciente> Pacientes { get; set; }
+    }
+}
